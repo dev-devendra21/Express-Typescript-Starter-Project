c206ecbdcfedc9d14dca29fb31aa37a86b46810e
devendra kumar <devendrakumaraa@gmail.com>
2025-06-02 15:37:03 +0530

    Added Winston log in mongodb

--- src/config/logger.config.ts
+++ src/config/logger.config.ts
@@ -1,33 +1,40 @@
 import winston from "winston";
+import "winston-mongodb";
 import { getCorrelationId } from "../utils/helpers/request.helpers";
 import DailyRotateFile from "winston-daily-rotate-file";
+import { serverConfig } from "./index";
 
 const logger = winston.createLogger({
-    format: winston.format.combine(
-        winston.format.timestamp({ format: "MM-DD-YYYY HH:mm:ss"  }), // how the timestamp should be formatted
-        winston.format.json(), // Format the log message as JSON
-        // define a cutom print
-        winston.format.printf( ({  level, message, timestamp, ...data }) => {
-            const output = { 
-                level,
-                message, 
-                timestamp, 
-                correlationId: getCorrelationId(), 
-                data 
-            };
-            return JSON.stringify(output);
-        })
-    ),
-    transports: [
-        new winston.transports.Console(),
-        new DailyRotateFile({
-            filename: "logs/%DATE%-app.log", // The file name pattern
-            datePattern: "YYYY-MM-DD", // The date format
-            maxSize: "20m", // The maximum size of the log file
-            maxFiles: "14d", // The maximum number of log files to keep
-        })
-        // TODO: add logic to integrate and save logs in mongo
-    ]
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "MM-DD-YYYY HH:mm:ss" }), // how the timestamp should be formatted
+    winston.format.json(), // Format the log message as JSON
+    // define a cutom print
+    winston.format.printf(({ level, message, timestamp, ...data }) => {
+      const output = {
+        level,
+        message,
+        timestamp,
+        correlationId: getCorrelationId(),
+        data,
+      };
+      return JSON.stringify(output);
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new DailyRotateFile({
+      filename: "logs/%DATE%-app.log", // The file name pattern
+      datePattern: "YYYY-MM-DD", // The date format
+      maxSize: "20m", // The maximum size of the log file
+      maxFiles: "14d", // The maximum number of log files to keep
+    }),
+    // TODO: add logic to integrate and save logs in mongo
+    new winston.transports.MongoDB({
+      db: serverConfig.MONGO_URI,
+      collection: "logs",
+      level: "info",
+    }),
+  ],
 });
 
 export default logger;
